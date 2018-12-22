d0ae0322ebe4267e682fd7ad986784721511dc7b
aarshic <aarshi.1998@gmail.com>
2018-12-22 17:47:17 +0530

    Adding the existing project

---
+++ New/app.js
@@ -0,0 +1,21 @@
+document.getElementById("myBtn").addEventListener("click", open_close);
+var menuState = 0 // close
+function open_close() {
+  if(menuState === 0){
+     menuState = 1;
+     document.getElementById("mySidenav").style.width = "200px";
+  }
+  else {
+     menuState = 0;
+     document.getElementById("mySidenav").style.width = "0";
+  }
+  console.log(menuState);
+} 
+
+$('ul.nav').find('a').click(function(){
+   var $href = $(this).attr('href');
+   var $anchor = $('#'+$href).offset();
+   // window.scrollTo($anchor.left,$anchor.top);
+   $('body').animate({ scrollTop: $anchor.top });
+   return false;
+});
